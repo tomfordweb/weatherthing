c3a15847b26e1edcf15297cbefb9cf7ce793eaeb
Tom Ford <tomfordweb@gmail.com>
2019-10-03 21:42:12 -0400

    dashboard test

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,42 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { LocationService, LocationStore, createInitialState } from '../set-location/state';
 
-describe('DashboardComponent', () => {
+describe('DashboardComponent - using store', () => {
   let component: DashboardComponent;
+  let locationStore: LocationStore;
   let fixture: ComponentFixture<DashboardComponent>;
+  let dom: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
+      providers: [
+        {
+          provide: LocationService,
+          useValue: jasmine.createSpyObj('TodosService', ['get'])
+        }
+      ],
+      declarations: [DashboardComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+    locationStore = TestBed.get(LocationStore);
+    dom = fixture.debugElement.nativeElement;
+    locationStore.update(createInitialState('Chicago'));
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should set location prop from store', () => {
+    expect(component.location).toBe('Chicago');
+  });
+
+  it('should display the location in the header', () => {
+    const h1 = dom.querySelector('h1');
+    expect(h1.textContent).toContain('Chicago');
   });
 });
+
